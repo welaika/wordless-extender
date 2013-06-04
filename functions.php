ec77513d4d44d83f794069ee4c8f5af0ea07e358
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2013-06-04 17:21:44 +0200

    function to remove Folders (recursive) and Files

--- functions.php
+++ functions.php
@@ -17,3 +17,23 @@ function wle_show_message($message, $is_error = false) {
 
   echo '<div id="message" class="' . $class . '"><p>' . $message . '</p></div>';
 }  
+
+// remove Folders (recursive) and Files
+function deleteDirAndFile($dirPath){
+  if (is_dir($dirPath)) {
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
+  } else {
+    unlink($dirPath);
+  }
+}

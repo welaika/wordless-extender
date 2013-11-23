b8199827d3643525cc9c4ec173dbb63dfacfe084
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-11-23 18:33:06 +0100

    just revisioning the view while refactoring the logic

--- constants.html.php
+++ constants.html.php
@@ -1,5 +1,5 @@
-<?php 
-  $cmanager = new WordlessExtenderConstantManager;
+<?php
+  $form = new WordlessExtenderConstantForm;
 ?>
 <!-- style for generate button -->
 <style type="text/css">
@@ -35,118 +35,50 @@
   </div>
 
   <h3>Constants list</h3>
-  <form method="post" action="">
+  <form method="post" action=" ">
     <table id="wordless-extender" class="wp-list-table widefat">
-    <thead>
-      <tr>
-        <th class="constant">Constant Name</th>
-        <th class="description">Description</th>
-        <th class="value">Value</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <?php $siteurl = $cmanager->init_constant('WP_SITEURL'); ?>
-        <td><?php echo $siteurl->name; ?></td>
-        <td><?php echo $siteurl->description; ?></td>
-        <td><input type="text" name="WP_SITEURL" value="<?php echo $siteurl->get_value(); ?>"></td>
-      </tr>
-      <!-- <tr>
-        <td>AUTH_KEY</td>
-        <td>define the AUTH_KEY. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='AUTH_KEY']" class="generate">Generate!</a></td>
-        <td><input type="text" name="AUTH_KEY" value="<?php echo get_option('WLE_AUTH_KEY'); ?>"></td>
-      </tr>
-      <tr>
-        <td>SECURE_AUTH_KEY</td>
-        <td>define the SECURE_AUTH_KEY. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='SECURE_AUTH_KEY']" class="generate">Generate!</a></td>
-        <td><input type="text" name="SECURE_AUTH_KEY" value="<?php echo get_option('WLE_SECURE_AUTH_KEY'); ?>"></td>
-      </tr>
-      <tr>
-        <td>LOGGED_IN_KEY</td>
-        <td>define the LOGGED_IN_KEY. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='LOGGED_IN_KEY']" class="generate">Generate!</a></td>
-        <td><input type="text" name="LOGGED_IN_KEY" value="<?php echo get_option('WLE_LOGGED_IN_KEY'); ?>"></td>
-      </tr>
-      <tr>
-        <td>NONCE_KEY</td>
-        <td>define the NONCE_KEY. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='NONCE_KEY']" class="generate">Generate!</a></td>
-        <td><input type="text" name="NONCE_KEY" value="<?php echo get_option('WLE_NONCE_KEY'); ?>"></td>
-      </tr>
-      <tr>
-        <td>AUTH_SALT</td>
-        <td>define the AUTH_SALT. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='AUTH_SALT']" class="generate">Generate!</a></td>
-        <td><input type="text" name="AUTH_SALT" value="<?php echo get_option('WLE_AUTH_SALT'); ?>"></td>
-      </tr>
-      <tr>
-        <td>SECURE_AUTH_SALT</td>
-        <td>define the SECURE_AUTH_SALT. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='SECURE_AUTH_SALT']" class="generate">Generate!</a></td>
-        <td><input type="text" name="SECURE_AUTH_SALT" value="<?php echo get_option('WLE_SECURE_AUTH_SALT'); ?>"></td>
-      </tr>
-      <tr>
-        <td>LOGGED_IN_SALT</td>
-        <td>define the LOGGED_IN_SALT. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='LOGGED_IN_SALT']" class="generate">Generate!</a></td>
-        <td><input type="text" name="LOGGED_IN_SALT" value="<?php echo get_option('WLE_LOGGED_IN_SALT'); ?>"></td>
-      </tr>
-      <tr>
-        <td>NONCE_SALT</td>
-        <td>define the NONCE_SALT. Here to generate it: <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a><a data-target="input[name='NONCE_SALT']" class="generate">Generate!</a></td>
-        <td><input type="text" name="NONCE_SALT" value="<?php echo get_option('WLE_NONCE_SALT'); ?>"></td>
-      </tr>
-      <tr>
-        <td>WPLANG</td>
-        <td>define the WPLANG. Here the WordPress languages: <a href="http://codex.wordpress.org/WordPress_in_Your_Language">http://codex.wordpress.org/WordPress_in_Your_Language</a></td>
-        <td><input type="text" name="WP_LANG" value="<?php echo get_option('WLE_WP_LANG'); ?>"></td>
-      </tr>
-      <tr>
-        <td>DISALLOW_FILE_EDIT</td>
-        <td>define the DISALLOW_FILE_EDIT. If TRUE you can't edit files in the WordPress backend.</td>
-        <td>
-          <?php 
-            $true_disallowfileedit = '';
-            $false_disallowfileedit = '';
-            if (get_option('WLE_DISALLOW_FILE_EDIT') == 'true') $true_disallowfileedit = 'checked';
-            elseif (get_option('WLE_DISALLOW_FILE_EDIT') == 'false') $false_disallowfileedit = 'checked';
-          ?>
-          <input type="radio" name="DISALLOW_FILE_EDIT" value="true" <?php echo $true_disallowfileedit ?>> True 
-          <input type="radio" name="DISALLOW_FILE_EDIT" value="false" <?php echo $false_disallowfileedit ?>> False 
-        </td>
-      </tr>
-      <tr>
-        <td>EMPTY_TRASH_DAYS</td>
-        <td>define the EMPTY_TRASH_DAYS. Must be an integer.</td>
-        <td><input type="text" name="EMPTY_TRASH_DAYS" value="<?php echo get_option('WLE_EMPTY_TRASH_DAYS'); ?>"></td>
-      </tr>
-      <tr>
-        <td>WP_ALLOW_REPAIR</td>
-        <td>define the WP_ALLOW_REPAIR. If TRUE allow automatic database optimizing.</td>
-        <td>
-          <?php 
-            $true_wpallowrepair = '';
-            $false_wpallowrepair = '';
-            if (get_option('WLE_WP_ALLOW_REPAIR') == 'true') $true_wpallowrepair = 'checked';
-            elseif (get_option('WLE_WP_ALLOW_REPAIR') == 'false') $false_wpallowrepair = 'checked';
-          ?>
-          <input type="radio" name="WP_ALLOW_REPAIR" value="true" <?php echo $true_wpallowrepair ?>> True 
-          <input type="radio" name="WP_ALLOW_REPAIR" value="false" <?php echo $false_wpallowrepair ?>> False 
-        </td>
-      </tr>
-      <tr>
+      <thead>
+        <tr>
+          <th class="constant">Constant Name</th>
+          <th class="description">Description</th>
+          <th class="value">Value</th>
+          <th class="value">Reset</th>
+          <th class="description">Ref.</th>
+          <th class="extra">Extra Controls</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+        $form->print_row('WP_SITEURL');
+        $form->print_row('AUTH_KEY');
+        $form->print_row('SECURE_AUTH_KEY');
+        $form->print_row('LOGGED_IN_KEY');
+        $form->print_row('NONCE_KEY');
+        $form->print_row('AUTH_SALT');
+        $form->print_row('SECURE_AUTH_SALT');
+        $form->print_row('LOGGED_IN_SALT');
+        $form->print_row('NONCE_SALT');
+        $form->print_row('WP_DEBUG', 'bool');
+        $form->print_row('DISALLOW_FILE_EDIT', 'bool');
+        $form->print_row(
+          'WPLANG',
+          'text',
+          'Set in the format <code>it_IT</code>'
+        );
+        $form->print_row(
+          'EMPTY_TRASH_DAYS',
+          'text',
+          'Use an integer to set the maximum trashed contents retention in <strong>days</strong>'
+        );
+        $form->print_row('DISABLE_WP_CRON', 'bool');
+        $form->print_row('WP_ALLOW_REPAIR', 'bool');
+
+      ?>
+      <!--
+
         <td>WP_POST_REVISIONS</td>
         <td>define the WP_POST_REVISIONS. Must be an integer.</td>
         <td><input type="text" name="WP_POST_REVISIONS" value="<?php echo get_option('WLE_WP_POST_REVISIONS'); ?>"></td>
-      </tr>
-      <tr>
-        <td>DISABLE_WP_CRON</td>
-        <td>define the DISABLE_WP_CRON. If TRUE disable cron.</td>
-        <td>
-          <?php 
-            $true_disablewpcron = '';
-            $false_disablewpcron = '';
-            if (get_option('WLE_DISABLE_WP_CRON') == 'true') $true_disablewpcron = 'checked';
-            elseif (get_option('WLE_DISABLE_WP_CRON') == 'false') $false_disablewpcron = 'checked';
-          ?>
-          <input type="radio" name="DISABLE_WP_CRON" value="true" <?php echo $true_disablewpcron ?>> True 
-          <input type="radio" name="DISABLE_WP_CRON" value="false" <?php echo $false_disablewpcron ?>> False 
-        </td>
       </tr>
       <tr>
         <td>FS_METHOD</td>
@@ -158,14 +90,18 @@
         <td>define the WP_CONTENT_DIR. Without /</td>
         <td><input type="text" name="WP_CONTENT_DIR" value="<?php echo get_option('WLE_WP_CONTENT_DIR'); ?>"></td>
       </tr> -->
+      </tbody>
     </table>
-    <p class="submit">
-      <input type="submit" id="submit" class="button-primary" value="Save constants">
-      Remember that a backup of wp-config is stored in a new file named wp-config-backup.php in site root.
-    </p>
+  <p class="submit">
+    <input type="submit" id="submit" class="button-primary" value="Save constants">
+    Remember that a backup of wp-config is stored in a new file named wp-config.php.bak in site root.
+  </p>
   </form>
   <form method="post">
-      <?php $cmanager->print_init_buttons(); ?>
+      <?php
+        $cmanager = WordlessExtenderConstantManager::get_instance();
+        $cmanager->print_init_buttons();
+      ?>
   </form>
 </div>
 

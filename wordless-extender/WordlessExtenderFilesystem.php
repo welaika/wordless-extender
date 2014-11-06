0799cbbabd1ced0bc3af1a579438a0f41f82ac85
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2014-11-06 14:46:42 +0100

    FileSystem interactions class

---
+++ wordless-extender/WordlessExtenderFilesystem.php
@@ -0,0 +1,72 @@
+<?php
+
+Class WordlessExtenderFilesystem{
+
+    public static function delete( $path )
+    {
+      if (is_dir($path)) {
+
+        if (substr($path, strlen($path) - 1, 1) != '/') {
+            $path .= '/';
+        }
+
+        $objects = glob($path . '*', GLOB_MARK);
+        foreach ($objects as $object) {
+            if (is_dir($object)) {
+                self::rrmdir($object);
+            } else {
+                unlink($object);
+            }
+        }
+        rmdir($path);
+
+      } elseif ( file_exists( $path ) ) {
+
+        unlink($path);
+
+      } else {
+
+        return false;
+
+      }
+    }
+
+    public static function backup_file( $file )
+    {
+        file_put_contents( "{$file}.orig", file_get_contents($file) );
+    }
+
+    public static function update_file( $file, $new_content )
+    {
+        file_put_contents( $file, $new_content );
+    }
+
+    public static function read_file( $file )
+    {
+        if (file_exists($file))
+            return file_get_contents( $file );
+        else
+            return false;
+    }
+
+    public static function backup_and_update_file( $file, $new_content )
+    {
+        self::backup_file($file);
+        self::update_file($file, $new_content);
+    }
+
+    private static function rrmdir($dir)
+    {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+                    }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+
+}

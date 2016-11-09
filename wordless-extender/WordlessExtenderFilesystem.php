a8afbcc8d2a6d590e3507a02691debdda38c1e16
Stefano Pau <stefano.pau@welaika.com>
2016-11-09 18:07:53 +0100

    fix rrmdir function call

--- wordless-extender/WordlessExtenderFilesystem.php
+++ wordless-extender/WordlessExtenderFilesystem.php
@@ -61,7 +61,7 @@ Class WordlessExtenderFilesystem{
             $objects = scandir($dir);
             foreach ($objects as $object) {
                 if ($object != "." && $object != "..") {
-                    if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+                    if (filetype($dir."/".$object) == "dir") self::rrmdir($dir."/".$object); else unlink($dir."/".$object);
                     }
             }
             reset($objects);
